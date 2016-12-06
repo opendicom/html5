2c6247b785bca7acf909b894c1300e7fd1a3648f
Claudio Baeza <cl.baeza@gmail.com>
2016-12-06 12:04:25 -0300

    html5cda
    Add installed apps, urls, views

--- html5/html5cda/urls.py
+++ html5/html5cda/urls.py
@@ -1,28 +1,6 @@
 from django.conf.urls import url, include
-from rest_framework import routers
 from html5cda import views
 
-router = routers.DefaultRouter()
-router.register(r'codesystem', views.CodesystemViewSet)
-router.register(r'scriptelement', views.ScriptelementViewSet)
-router.register(r'header', views.HeaderViewSet)
-router.register(r'footer', views.FooterViewSet)
-router.register(r'articlehtml', views.ArticlehtmlViewSet)
-router.register(r'code', views.CodeViewSet)
-router.register(r'estudio', views.EstudioViewSet)
-router.register(r'plantilla', views.PlantillaViewSet)
-router.register(r'plantillagruposldap', views.PlantillagruposldapViewSet)
-router.register(r'seccion', views.SeccionViewSet)
-router.register(r'entry', views.EntryViewSet)
-router.register(r'qualifier', views.QualifierViewSet)
-router.register(r'value', views.ValueViewSet)
-router.register(r'autenticado', views.AutenticadoViewSet)
-router.register(r'sec', views.SecViewSet)
-router.register(r'subsec', views.SubsecViewSet)
-router.register(r'subsubsec', views.SubsubsecViewSet)
-router.register(r'firma', views.FirmaViewSet)
-router.register(r'submit', views.SubmitViewSet)
-
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^editor', views.editor, name='editor'),
 ]
--- html5/html5cda/views.py
+++ html5/html5cda/views.py
@@ -1,1 +1,9 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from html5cda import models
+
+
+@login_required(login_url='/html5dicom/login')
+def editor(request, *args, **kwargs):
+    context_user = {'context': 'ok'}
+    return render(request, template_name='html5cda/editor.html', context=context_user)
