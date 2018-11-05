fe15262efcbe16a4e97c2049a72753c64262a017
Claudio Baeza <cl.baeza@gmail.com>
2018-11-05 11:29:48 -0300

    Delete old Cornerstone html5dicom

--- html5/html5dicom/static/html5dicom/cornerstone/js/about.js
+++
@@ -1,6 +0,0 @@
-loadTemplate("templates/about.html", function(element) {
-    $('body').append(element);
-    $("#about").click(function() {
-        $("#aboutModal").modal();
-    });
-});
--- html5/html5dicom/static/html5dicom/cornerstone/js/help.js
+++
@@ -1,6 +0,0 @@
-loadTemplate("templates/help.html", function(element) {
-    $('body').append(element);
-    $("#help").click(function() {
-        $("#helpModal").modal();
-    });
-});
