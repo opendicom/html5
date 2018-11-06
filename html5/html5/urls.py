264a2c46f9b0ef21a18858e33d8b18d9664e31f8
Claudio Baeza <cl.baeza@gmail.com>
2018-11-06 15:18:02 -0300

    Default load view html5dicom

--- html5/html5/urls.py
+++ html5/html5/urls.py
@@ -1,29 +1,16 @@
-"""html5 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
+from html5dicom import views
 from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Proxy Dicom Web')
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.user_login),
     url(r'^html5dicom/', include('html5dicom.urls')),
     url(r'^html5cda/', include('html5cda.urls')),
     url(r'^proxydcmweb/', include('proxyrest.urls')),
     url(r'^accounts/', include('accounts.urls')),
+    url(r'^admin/', admin.site.urls),
     url(r'^docs/$', schema_view)
 ]
