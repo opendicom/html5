28434e9fa919a5c00c3040c9bc2356eea81f3ba4
Claudio Baeza <cl.baeza@gmail.com>
2019-10-30 17:44:24 -0300

    Docker

---
+++ html5/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "mariadb" ]
+then
+    echo "Waiting for mariadb..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "MariDB started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+python manage.py collectstatic --no-input --clear
+
+exec "$@"
