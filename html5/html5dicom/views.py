3fd546afa4a1f720e4b51230b9dcdb29271e770a
Claudio Baeza <cl.baeza@gmail.com>
2020-04-06 12:56:20 -0300

    Revert "Comment streaming zip"
    This reverts commit 11badc3abd75eeb064995ae17e9e688683605c43.

--- html5/html5dicom/views.py
+++ html5/html5dicom/views.py
@@ -415,18 +415,10 @@ def study_token_zip(request, *args, **kwargs):
                 study_token['SeriesInstanceUID'] = request.GET['SeriesInstanceUID']
             url_zip = settings.HTTP_DICOM + '/studyToken?' + urllib.parse.urlencode(study_token,
                                                                                     quote_via=urllib.parse.quote)
-            #r = StreamingHttpResponse(stream_response(url_zip))
-            #r['Content-Type'] = "application/zip"
-            #r['Content-Disposition'] = "attachment; filename=dcm.zip"
-            #return r
-
-            response_study_token = requests.get(url_zip)
-            response = HttpResponse(response_study_token.content,
-                                    status=response_study_token.status_code,
-                                    content_type="application/zip")
-            response['Content-Disposition'] = "attachment; filename=dcm.zip"
-            return response
-
+            r = StreamingHttpResponse(stream_response(url_zip))
+            r['Content-Type'] = "application/zip"
+            r['Content-Disposition'] = "attachment; filename=dcm.zip"
+            return r
         except (Session.DoesNotExist, KeyError):
             raise PermissionDenied
     else:
